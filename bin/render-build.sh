fd2c8c9a86bad273e27474cb5d63a1652cb03700
masahirog <masahiro11g@gmail.com>
2025-06-15 17:42:07 +0900

    no message

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,14 +2,10 @@
 # exit on error
 set -o errexit
 
-# Install wkhtmltopdf
-apt-get update -qq && apt-get install -y wkhtmltopdf
+# Install PostgreSQL client and wkhtmltopdf
+apt-get update -qq && apt-get install -y libpq-dev postgresql-client wkhtmltopdf
 
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
-EOF
-
-# 実行権限を付与
-chmod a+x bin/render-build.sh
