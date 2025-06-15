ebf749711feb7a295579397be465c2510a5ce069
masahirog <masahiro11g@gmail.com>
2025-06-16 08:32:03 +0900

    no message

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -22,3 +22,4 @@ bundle exec rake assets:clean
 # データベースの作成とマイグレーション
 bundle exec rake db:create RAILS_ENV=production || true
 bundle exec rake db:migrate RAILS_ENV=production
+bundle exec rake db:seed RAILS_ENV=production
