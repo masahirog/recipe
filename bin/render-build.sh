cfcc706152fb9ea30421b39209c7ca322fd2ae5e
masahirog <masahiro11g@gmail.com>
2025-06-15 20:37:33 +0900

    no message

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,7 +5,12 @@ set -o errexit
 # Install PostgreSQL client and wkhtmltopdf
 apt-get update -qq && apt-get install -y libpq-dev postgresql-client wkhtmltopdf
 
+# Set library path for PostgreSQL
+export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH
+
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-bundle exec rake db:migrate
+
+# Skip database migration for now to avoid pg gem loading issue
+# bundle exec rake db:migrate
